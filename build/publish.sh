ee1494eea0900c4256601aed44e779fa249e9f2b
Ivaylo Nikolov <inikolov@sonicomit.com>
2024-11-06 07:43:28 +0200

    MTOCAPPS-319 - Hints for build

--- build/publish.sh
+++ build/publish.sh
@@ -9,14 +9,7 @@ git checkout -b build
 jake build[,,true]
 jake docs
 
-git add \
-    dist/leaflet.draw.js \
-    dist/leaflet.draw-src.js \
-    dist/leaflet.draw-src.map \
-    dist/leaflet.draw.css \
-    dist/leaflet.draw-src.css \
-    docs/* \
-    -f
+git add dist/leaflet.draw.js dist/leaflet.draw-src.js dist/leaflet.draw-src.map dist/leaflet.draw.css dist/leaflet.draw-src.css docs/* -f
 
 git commit -m "v$VERSION"
 
@@ -27,3 +20,6 @@ npm publish
 
 git checkout master
 git branch -D build
+
+npm install --save "git://github.com/vemtechnology/Leaflet.draw/commits/v1.0.4-vem.1"
+npm install --save "git://github.com/vemtechnology/Leaflet.draw#v1.0.4-vem.1"
